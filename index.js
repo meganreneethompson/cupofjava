e2de14bcbb98618336a5a70af89209d25a3975b7
Meg Thompson <128401836+meganreneethompson@users.noreply.github.com>
2024-02-15 15:56:11 -0500

    toggles

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+// DOM Elements
 
-   // DOM Elements
 const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
 const solarButton = document.getElementById('solar');
 const body = document.body;
 
+
 // Apply the cached theme on reload
+
 const theme = localStorage.getItem('theme');
 const isSolar = localStorage.getItem('isSolar');
+
 if (theme) {
   body.classList.add(theme);
   isSolar && body.classList.add('solar');
@@ -16,17 +19,34 @@ if (theme) {
 // Button Event Handlers
 
 darkButton.onclick = () => {
-  body.classList.remove('light');
-  body.classList.add('dark');
-
   body.classList.replace('light', 'dark');
   localStorage.setItem('theme', 'dark');
 };
 
 lightButton.onclick = () => {
-  body.classList.remove('dark');
-  body.classList.add('light');
   body.classList.replace('dark', 'light');
 
   localStorage.setItem('theme', 'light');
+};
+
+solarButton.onclick = () => {
+
+  if (body.classList.contains('solar')) {
+    
+    body.classList.remove('solar');
+    solarButton.style.cssText = `
+    --bg-solar: var(--yellow);`
+
+    localStorage.removeItem('isSolar');
+
+  } else {
+solarButton.style.cssText = `
+--bg-solar: white;`
+
+body.classList.add('solar');
+solarButton.innerText = 'solarize';
+    localStorage.setItem('isSolar', true);
+
+   
+  }
 };
